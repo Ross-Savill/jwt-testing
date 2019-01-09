61748f3b812ef1f10f47b5abc78159c19f0e87a3
Ross Savill <rosssavill@Rosss-MacBook-Air.local>
2019-01-09 16:46:41 +1100

    registration added

--- controllers/auth.js
+++ controllers/auth.js
@@ -32,5 +32,35 @@ router.post('/login', (req, res) => {
   }
 });
 
+router.post('/register', (req, res) => {
+  console.log(req.body)
+  const { username, password } = req.body
+  if (username && password) {
+    User.findOne({ username })
+      .then(doc => {
+        if (doc) {
+          console.log("DOC FOUND")
+          res.status(409).send('Username already taken')
+        }
+        else {
+          console.log("DOC is NULL")
+          User.create({ username, password })
+            .then(doc => {
+              console.log("USER CREATED")
+              const token = generateToken(doc)
+              return res.send({ token })
+            })
+            .catch(err => {
+              console.log("create user failed")
+              res.send("creating user failed")
+            })
+        }
+      })
+      .catch(err => {
+        res.send("Error")
+      })
+  } 
+})
+
 
 module.exports = router;
