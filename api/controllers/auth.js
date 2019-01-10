ac27584898df28cc6f9660d6e5814e467adc6048
Ross Savill <rosssavill@Rosss-MacBook-Air.local>
2019-01-10 14:54:39 +1100

    fe made

---
+++ api/controllers/auth.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt'); // BCRYPT
+
+const User = require('../models/User'); 
+const saltRounds = 10; //BCRYPT
+
+const generateToken = (user) => {
+  const token = jwt.sign(
+    { username: user.username },
+    'coder-academy', // jwt secret
+    { expiresIn: '1h' }
+  );
+  return token;
+}
+
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  if (username) {
+    User.findOne({ username })
+      .then(doc => {
+        if (!doc) {
+          return res.status(403).send('Bad credentials');
+        }
+        if (doc.password !== password) {
+          return res.status(403).send('Bad credentials');
+        }
+        const token = generateToken(doc);
+        return res.send({ token });
+      });
+  } else {
+    return res.status(403).send('Bad credentials');
+  }
+});
+
+router.post('/register', (req, res) => {
+  console.log(req.body)
+  const { username, password } = req.body
+  if (username && password) {
+    User.findOne({ username })
+      .then(doc => {
+        if (doc) {
+          console.log("DOC FOUND")
+          res.status(409).send('Username already taken')
+        }
+        else {
+          console.log("DOC is NULL")
+          User.create({ username, password })
+            .then(doc => {
+              console.log("USER CREATED")
+              const token = generateToken(doc)
+              return res.send({ token })
+            })
+            .catch(err => {
+              console.log("create user failed")
+              res.send("creating user failed")
+            })
+        }
+      })
+      .catch(err => {
+        res.send("Error")
+      })
+  } 
+})
+
+
+module.exports = router;
---
+++ fe/src/Form.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Form extends Component {
+  state = {};
+  handleInputChange = (e) => {
+    const { value, id } = e.currentTarget;
+    this.setState({ [id]: value });
+  }
+  submitForm = (e) => {
+    e.preventDefault();
+    const { username, password } = this.state;
+    const url = "http://localhost:8000/auth/login";
+    const data = {
+      username,
+      password
+    }
+    axios.post(url, data)
+      .then(resp => {
+        const { token } = resp.data;
+        localStorage.setItem('token', token);
+        // RE-ROUTE TO HOMEPAGE
+        this.setState({ message: 'Correct username or password...moron', error: undefined });
+      })
+      .catch(error => {
+        const { status } = error.response;
+        if (status === 403) {
+          this.setState({ error: 'Incorrect username or password. Try again...moron', message: undefined });
+        }
+      });
+  }
+
+  handleProtectedRequest = (e) => {
+    // e.preventDefault();
+    const token = localStorage.getItem('token');
+    if (token) {
+      const url = "http://localhost:8000/protected/resources";
+      const options = {
+        headers: {
+          token
+        }
+      }
+      axios.get(url, options)
+        .then(resp => {
+          this.setState({ message: resp.data, error: undefined });
+        })
+        .catch(error => {
+          this.setState({ error: error.response, message: undefined });
+        })
+    } else {
+      this.setState({ error: 'Not authenticated. Please log in', message: undefined })
+    }
+  }
+
+  render() {
+    const { error, message } = this.state;
+    return (
+      <>
+        <form>
+          <label htmlFor="username">Username: </label>
+          <input type="text" id="username" onChange={this.handleInputChange} />
+          <label htmlFor="password">Password: </label>
+          <input type="password" id="password" onChange={this.handleInputChange} />
+          <button onClick={this.submitForm}>Login</button>
+        </form>
+        { error && <p>{ error }</p> }
+        { message && <p>{ message }</p> }
+        <button onClick={this.handleProtectedRequest}>Request protected</button>
+      </>
+    );
+  }
+}
+
+export default Form;
